a5bba3f9da42b665925aca47b2ae8c078217c315
MadSons <madsenc004@gmail.com>
2024-01-31 17:35:40 -0500

    setup.py change

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
 setup(
-    name = 'promptmove-ai',
-    packages = ['pip', 'numpy', 'matplotlib', 'pandas'], 
-    # packages list needs to be updated later
-    version = '0.1',
-    license = 'MIT',
+    name='promptmove-ai',
+    packages=find_packages(),  # automatically find all packages
+    version='0.1',
+    license='MIT',
     long_description=open('README.md').read(),
+    install_requires=[
+        'pip',
+        'numpy',
+        'matplotlib',
+        'pandas',
+    ],  # list your dependencies here
 )
