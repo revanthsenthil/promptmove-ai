0ca5e869f8592638c251972fe8107bfd349f2ccd
Calvin Madsen <cfmadsen@purdue.edu>
2024-02-23 00:05:05 -0500

    Google Cloud speech to text integration with streamlit. Llama attempt (server error) (#7)
    * streamlit base enter input
    
    * google cloud speech to text with integrated with streamlit
    
    * google cloud speech to text with streamlit
    
    * comments to give Google credit
    
    * fix CI, need to test Google Cloud
    
    * google cloud api test
    
    * Update actions.yml
    
    * CI build with -e
    
    * CI build with -e
    
    * CI build with -e
    
    * google cloud ai script. Still dont working

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
+
+def get_requirements(path: str):
+    return [req.strip() for req in open(path)]
+
+
 setup(
     name='promptmove-ai',
     packages=find_packages(),  # automatically find all packages
     version='0.1',
     license='MIT',
     long_description=open('README.md').read(),
-    install_requires=[
-        'pip',
-        'numpy',
-        'matplotlib',
-        'pandas',
-    ],  # list your dependencies here
+    install_requires=get_requirements("requirements.txt")
 )
