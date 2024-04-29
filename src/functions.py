2c92b81e3973f399ef87082ea493fcc48ed7ff5c
MadSons <madsenc004@gmail.com>
2024-04-28 23:07:12 -0400

    script gen fix

--- src/functions.py
+++ src/functions.py
@@ -9,8 +9,6 @@ import json
 ACTIONS = []
 OBJECTS = []
 GRAPH = None
-COMM = None
-
 
 def perform_action_on_object(action, object):
     """Perform an action on an object in a virtual home environment"""
@@ -38,19 +36,23 @@ def perform_action_on_object(action, object):
     log(f"Appended {action} on {object} to the list")
     return json.dumps({"action": action, "object": object, "status": "success"})
 
-def run_script(date : str, frame_rate : int = 10, image_width : int = 320, image_height : int = 240, actions=ACTIONS, objects=OBJECTS, no_graphics=False):
+def run_script(date : str, frame_rate : int = 10, image_width : int = 320, image_height : int = 240, actions=[], objects=[], no_graphics=False, testing=False):
     """Run the scrupt for performing actions on objects in a virtual home environment"""
-    global ACTIONS, OBJECTS, GRAPH, COMM
-    log(f"Running run_script({actions}, {objects})")
+    global ACTIONS, OBJECTS, GRAPH
+    if testing:
+        ACTIONS = actions
+        OBJECTS = objects
 
-    if len(actions) == 0 and len(objects) == 0:
+    log(f"Running run_script({ACTIONS}, {OBJECTS})")
+
+    if len(ACTIONS) == 0 and len(OBJECTS) == 0:
         log(json.dumps({"success_actions": [], "success_objects": [], 'failed_actions': [], 'failed_objects': []}))
         return
     
-    # Initialize Unity COMMunication
+    # Initialize Unity communication
     file_name = "linux_exec/linux_exec.v2.3.0.x86_64" # path to executable
     try:
-        COMM = UnityCommunication(file_name=file_name, port="8081", x_display='0', timeout_wait=120, no_graphics=no_graphics)
+        comm = UnityCommunication(file_name=file_name, port="8081", x_display='0', timeout_wait=120, no_graphics=no_graphics)
         log("Connected to Unity")
     except Exception as e:
         log(f"Failed to connect to Unity: {e}")
@@ -61,18 +63,17 @@ def run_script(date : str, frame_rate : int = 10, image_width : int = 320, image
     # Base Environment Setup
     if GRAPH is None:
         log("Creating the scene")
-        COMM.reset(4)
-        COMM.add_character('chars/Male1', initial_room='kitchen')
-        COMM.add_character('chars/Female2', position=[0, 0, 0])
+        comm.reset(4)
+        comm.add_character('chars/Male1', initial_room='kitchen')
+        comm.add_character('chars/Female2', position=[0, 0, 0])
     else:
         log("Expanding the scene")
-        COMM.reset(4)
-        COMM.expand_scene(GRAPH)
+        comm.reset(4)
+        comm.expand_scene(GRAPH)
 
     # get and save environmet graph
-    _, g = COMM.environment_graph()
-    GRAPH = g
-    log(f"Initial character position: {g['nodes'][0]['obj_transform']['position']}")
+    _, g1 = comm.environment_graph()
+    log(f"Initial character position: {g1['nodes'][0]['obj_transform']['position']}")
 
     # Find the nodes for the objects and actions
     script = []
@@ -81,16 +82,16 @@ def run_script(date : str, frame_rate : int = 10, image_width : int = 320, image
 
     # put the user on the sofa
     try:
-        sofa = find_nodes(g, class_name='sofa')[0]
+        sofa = find_nodes(g1, class_name='sofa')[0]
     except IndexError:
         log("Object sofa not found in the environment")
     else:
         script.append('<char1> [{}] <{}> ({})'.format('sit', 'sofa', sofa['id']))
 
     # append the script for the actions and objects
-    for obj, act in zip(objects, actions):
+    for obj, act in zip(OBJECTS, ACTIONS):
         try:
-            object = find_nodes(g, class_name=obj)[0]
+            object = find_nodes(g1, class_name=obj)[0]
         except IndexError:
             log(f"Object {obj} not found in the environment")
             continue
@@ -103,14 +104,14 @@ def run_script(date : str, frame_rate : int = 10, image_width : int = 320, image
     # if the script is empty, return the list of successful and failed actions and objects
     if len(script) == 1:
         log(json.dumps({"success_actions": [], "success_objects": [], 'failed_actions': [], 'failed_objects': []}))
-        COMM.close()
+        comm.close()
         ACTIONS = []
         OBJECTS = []
         return
 
     # render the images from the script
     try:
-        _, _ = COMM.render_script(script=script,
+        _, _ = comm.render_script(script=script,
                                             frame_rate=frame_rate, # linear time increase
                                             processing_time_limit=60,
                                             find_solution=False,
@@ -122,16 +123,17 @@ def run_script(date : str, frame_rate : int = 10, image_width : int = 320, image
                                             file_name_prefix=date)
     except Exception as e:
         log(f"Failed to render the script: {e}")
-        COMM.close()
+        comm.close()
         return
     
     # update environemnt graph
-    _, g = COMM.environment_graph()
+    _, g2 = comm.environment_graph()
+    print(g2)
     try:
-        GRAPH = g
-        log(f"Final Character position: {GRAPH['nodes'][0]['obj_transform']['position']}")
+        GRAPH = g2
+        log(f"Final Character position: {g2['nodes'][0]['obj_transform']['position']}")
     except IndexError:
-        GRAPH = None
+        GRAPH = g1
         log("Character position not found in the environment")
 
     # output paths
@@ -150,9 +152,9 @@ def run_script(date : str, frame_rate : int = 10, image_width : int = 320, image
     utils_viz.generate_video(input_path=path_video, prefix=date, output_path=video_output_path)
 
     # reset everything
-    COMM.close()
-    failed_actions = [act for act in actions if act not in success_actions]
-    failed_objects = [obj for obj in objects if obj not in success_objects]
+    comm.close()
+    failed_actions = [act for act in ACTIONS if act not in success_actions]
+    failed_objects = [obj for obj in OBJECTS if obj not in success_objects]
     ACTIONS = []
     OBJECTS = []
     log(json.dumps({"success_actions": success_actions, "success_objects": success_objects, 'failed_actions': failed_actions, 'failed_objects': failed_objects}))
@@ -161,7 +163,7 @@ def run_script(date : str, frame_rate : int = 10, image_width : int = 320, image
 if __name__ == "__main__":
     import datetime
     date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    perform_action_on_object("walk", "fridge")
-    perform_action_on_object("open", "fridge")
+    perform_action_on_object("walk", "curtains")
+    perform_action_on_object("open", "curtains")
 
     run_script(date)
